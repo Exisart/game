987b7615492faec582e046cb441a5538b6a0f242
Exisart <greshnik27@ukr.net>
2016-05-29 00:02:13 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,22 +1,47 @@
 // ========== create perce
 
+$('#create').click(function(){
+	localStorage.setItem('name', "");
+	localStorage.setItem('force', 25);
+	localStorage.setItem('agility', 25);
+	localStorage.setItem('speed', 25);
+	localStorage.setItem('stamina', 25);
+})
 // ========== edit perce
 
-var name 	= $('#name').value;
-var force 	= $('#force').value;
-var agility = $('#agility').value;
-var speed 	= $('#speed').value;
-var stamina = $('#stamina').value;
-
+$('#save').click(function(){
+	localStorage.setItem('name', $('#name').val());
+	localStorage.setItem('force', $('#force').val());
+	localStorage.setItem('agility', $('#agility').val());
+	localStorage.setItem('speed', $('#speed').val());
+	localStorage.setItem('stamina', $('#stamina').val());
+})
 $('#clear').click(function(){
-	$('#name').val("name");
-	$('#force').value 	= 25;
-	$('#agility').value = 25;
-	$('#speed').value 	= 25;
-	$('#stamina').value = 25;
+	localStorage.setItem('name', "");
+	localStorage.setItem('force', 25);
+	localStorage.setItem('agility', 25);
+	localStorage.setItem('speed', 25);
+	localStorage.setItem('stamina', 25);
 })
 
+var name, force, agility, speed, stamina;
+function getParam(){
+	name = localStorage.getItem('name');
+	$('#name').val(name);
 
+	force = localStorage.getItem('force');
+	$('#force').val(force);
+
+	agility = localStorage.getItem('agility');
+	$('#agility').val(agility);
+
+	speed = localStorage.getItem('speed');
+	$('#speed').val(speed);
+
+	stamina = localStorage.getItem('stamina');
+	$('#stamina').val(stamina);
+}
+$(document).ready(getParam());
 
 // ========== create opponent
 
@@ -25,3 +50,5 @@ $('#clear').click(function(){
 // ========== loot
 
 // ========== edit pers with bonus
+
+
