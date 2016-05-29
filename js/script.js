a3a3f46901ce1878c6c280ae22ebd4a5567e1ce2
Exisart <greshnik27@ukr.net>
2016-05-29 19:57:35 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,45 +1,67 @@
-// ========== create perce
+// ========== menu
+
+$('#main').click(function goMain(){
+	$('.visible').removeClass('visible');
+	$('#perc_param').addClass('visible');
+	getParam();
+});
 
 $('#create').click(function(){
+	$('.visible').removeClass('visible');
+	$('#edit').addClass('visible');
+	getParam();
+});
+
+// ========== create perce
+$('#create').click(function create(){
 	localStorage.setItem('name', "");
 	localStorage.setItem('force', 25);
 	localStorage.setItem('agility', 25);
 	localStorage.setItem('speed', 25);
 	localStorage.setItem('stamina', 25);
-})
-// ========== edit perce
+	getParam();
+});
 
-$('#save').click(function(){
+// ========== edit perce
+$('#save').click(function save(){
 	localStorage.setItem('name', $('#name').val());
 	localStorage.setItem('force', $('#force').val());
 	localStorage.setItem('agility', $('#agility').val());
 	localStorage.setItem('speed', $('#speed').val());
 	localStorage.setItem('stamina', $('#stamina').val());
-})
-$('#clear').click(function(){
+	getParam();
+});
+
+$('#clear').click(function clear(){
 	localStorage.setItem('name', "");
 	localStorage.setItem('force', 25);
 	localStorage.setItem('agility', 25);
 	localStorage.setItem('speed', 25);
 	localStorage.setItem('stamina', 25);
-})
+	getParam();
+});
 
 var name, force, agility, speed, stamina;
 function getParam(){
 	name = localStorage.getItem('name');
 	$('#name').val(name);
+	$('#perc_name').html(name);
 
 	force = localStorage.getItem('force');
 	$('#force').val(force);
+	$('#perc_force').html(force);
 
 	agility = localStorage.getItem('agility');
 	$('#agility').val(agility);
+	$('#perc_agility').html(agility);
 
 	speed = localStorage.getItem('speed');
 	$('#speed').val(speed);
+	$('#perc_speed').html(speed);
 
 	stamina = localStorage.getItem('stamina');
 	$('#stamina').val(stamina);
+	$('#perc_stamina').html(stamina);
 }
 $(document).ready(getParam());
 
